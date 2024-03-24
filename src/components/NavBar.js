7dd4170e69f12295e4aa09f6b162e251560b9514
Samuel Ercanbrack <sam.ercanbrack@outlook.com>
2024-03-24 17:42:32 -0600

    changed sizing of integ detailing logo on large screens

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ export default function NavBar() {
     return (
             <nav className="w-screen flex justify-between items-center flex-wrap bg-teal-600 xxs:p-2 sm:pb-14 sm:pt-14 lg:p-10 lg:pb-16 lg:pt-16">
                 <Link href="/"className="flex items-center flex-shrink-0 text-white mr-6">
-                    <span className='font-semibolt xxs:text-2xl xs:text-3xl sm:text-4xl sm:pl-2 lg:pb-4 lg:pt-4 lg:text-7xl tracking-tight'>Integ Detailing</span>
+                    <span className='font-semibolt xxs:text-2xl xs:text-3xl sm:text-4xl sm:pl-2 lg:pb-4 lg:pt-4 lg:text-6xl tracking-tight'>Integ Detailing</span>
                 </Link>
                 <HamburgerMenu />
             </nav>
